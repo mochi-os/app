b3577cfed7c26ffb262013e82211ab9a43b84732
Alistair Cunningham <acunningham@integrics.com>
2025-12-09 18:02:25 +0900

    Remove apps list from sidebar

--- web/src/components/layout/data/sidebar-data.ts
+++ web/src/components/layout/data/sidebar-data.ts
@@ -1,17 +1,5 @@
-import { Package } from 'lucide-react'
 import { type SidebarData } from '../types'
 
 export const sidebarData: SidebarData = {
-  navGroups: [
-    {
-      title: 'Publisher',
-      items: [
-        {
-          title: 'My Apps',
-          url: '/',
-          icon: Package,
-        },
-      ],
-    },
-  ],
+  navGroups: [],
 }
